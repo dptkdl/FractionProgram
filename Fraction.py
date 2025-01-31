00225617837aa24ea72a82a80283d171eadc4ff2
shamika-anne <shamanne.sawalha@gmail.com>
2025-02-01 06:51:25 +0800

    added conditions for initialization function

--- Fraction.py
+++ Fraction.py
@@ -1,8 +1,15 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
-        pass
+        if denominator == 0:
+            raise ZeroDivisionError
+        elif isinstance(numerator, int):
+            self.numerator = numerator
+            self.denominator = denominator
+        elif isinstance(numerator, str):
+            separated = numerator.split("/")
+            self.numerator = int(separated[0])
+            self.denominator = int(separated[1])
 
     def gcd(a, b):
         #TODO
