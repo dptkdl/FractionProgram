23d1c7a7dc2c239e746c68d9765f6548e116c28b
shamika-anne <shamanne.sawalha@gmail.com>
2025-02-01 21:17:24 +0800

    changed unnecessary elifs to ifs

--- Fraction.py
+++ Fraction.py
@@ -55,8 +55,8 @@ class Fraction(object):
     def get_fraction(self):
         if Fraction.gcd(self.numerator, self.denominator) == 0:
             return "0"
-        elif Fraction.get_denominator(self) < 0 and Fraction.get_numerator(self) > 0:
+        if Fraction.get_denominator(self) < 0 and Fraction.get_numerator(self) > 0:
             return f"-{self.get_numerator()}/{abs(self.get_denominator())}"
-        elif Fraction.get_denominator(self) < 0 and Fraction.get_numerator(self) < 0:
+        if Fraction.get_denominator(self) < 0 and Fraction.get_numerator(self) < 0:
             return f"{abs(self.get_numerator())}/{abs(self.get_denominator())}"
         return f"{self.get_numerator()}/{self.get_denominator()}"
